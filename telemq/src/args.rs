a6a20ff0408d45e0a97ade32d357632801b54f6b
Alex Pikalov <alex.pikalov.khar@gmail.com>
2021-12-26 14:56:05 +0100

    chore: update clap to 3.0.0-beta.8

--- telemq/src/args.rs
+++ telemq/src/args.rs
@@ -6,12 +6,12 @@ pub fn parse_args() -> ArgMatches {
             Arg::new("CONFIG_FILE")
                 .short('c')
                 .long("config")
-                .about("TeleMQ configuration file")
+                .help("TeleMQ configuration file")
                 .takes_value(true),
         )
         .arg(
             Arg::new("TCP_PORT")
-                .about("TCP port TeleMQ will start listening on.")
+                .help("TCP port TeleMQ will start listening on.")
                 .index(1),
         )
         .get_matches()
